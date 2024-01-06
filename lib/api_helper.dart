aedc44408c338ecf9ea0e5d848c665a955daf7ae
GuilhermeBatist <guilhermeb.ika62@gmail.com>
2024-01-06 18:23:31 +0000

    bug autocomplete resosolvido

--- lib/api_helper.dart
+++ lib/api_helper.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
 import 'dart:convert';
-import 'package:http/http.dart' as http;
-
-Future<List<String>> fetchAutocomplete(String query) async {
-  final response = await http.get('https://api.scryfall.com/cards/autocomplete?q=$query' as Uri);
-
-  if (response.statusCode == 200) {
-    // Parse a resposta JSON
-    final List<dynamic> data = json.decode(response.body);
-    // Retorne a lista de resultados de autocomplete
-    return List<String>.from(data);
-  } else {
-    // Se a requisição falhar, lança uma exceção
-    throw Exception('Falha na requisição');
-  }
-}
 
 class AutocompleteWidget extends StatefulWidget {
   @override
@@ -34,29 +18,58 @@ class _AutocompleteWidgetState extends State<AutocompleteWidget> {
         TextField(
           controller: _controller,
           onChanged: (value) async {
-            // Faça a chamada da função de autocomplete ao digitar
             List<String> results = await fetchAutocomplete(value);
             setState(() {
               _autocompleteResults = results;
             });
           },
-          decoration: InputDecoration(labelText: 'Digite algo'),
+          decoration: InputDecoration(labelText: 'Enter something'),
         ),
         SizedBox(height: 10),
-        // Exiba os resultados de autocomplete
-        Expanded(
-          child: ListView.builder(
-            itemCount: _autocompleteResults.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(_autocompleteResults[index]),
-              );
-            },
+        if (_autocompleteResults.isNotEmpty)
+          Container(
+            height: 200,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            child: ListView.builder(
+              itemCount: _autocompleteResults.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_autocompleteResults[index]),
+                  onTap: () {
+                    setState(() {
+                      _controller.text = _autocompleteResults[index];
+                      _autocompleteResults.clear();
+                    });
+                  },
+                );
+              },
+            ),
           ),
-        ),
       ],
     );
   }
 }
 
+Future<List<String>> fetchAutocomplete(String query) async {
+  final response = await http.get(Uri.parse('https://api.scryfall.com/cards/autocomplete?q=$query'));
 
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> data = json.decode(response.body);
+    final List<dynamic>  suggeestions = data['data'];
+    return List<String>.from(suggeestions);
+  } else {
+    throw Exception('Failed to fetch autocomplete data');
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: AutocompleteWidget(),
+    ),
+  ));
+}
